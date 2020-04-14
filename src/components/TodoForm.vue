c9ae30ad5db1ed2422e7b6546ad15b93d50e118b
unknown <gabriel_picaz@estudante.sc.senai.br>
2020-04-14 16:25:18 -0300

    tarefa 2

---
+++ src/components/TodoForm.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="Form">
+  <input id="texto" type="text" placeholder="Nova Tarefa?" v-model="novaTarefa">
+  <button id="botao" @click="adicionarTarefa">OK</button>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "TodoForm",
+    data: function(){
+        return{
+            novaTarefa: ''
+        }
+    }, methods:{
+        adicionarTarefa: function(){
+            this.$emit('addTarefa',{novaTarefa: this.novaTarefa})
+        }
+    }
+}
+</script>
+
+<style>
+    #Form{
+        background-color: rgb(236, 119, 9);
+        width: 500px;
+        height: 170px;
+        margin: 50px auto 0px auto;
+        border: 2px black solid;
+        border-radius: 20px;
+    }
+
+    #texto{
+        margin: 80px 0 0 27%;
+        height: 20px;
+        width: 200px;
+        border: black 2px solid;
+    }
+
+    #botao{
+       width: 50px;
+       height: 20px;
+       color: red;
+       outline: none;
+       cursor: pointer;
+       margin: 0px 5px;
+    }
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="todoList">
+      <div class="tarefasCaixas" v-for="(todoList, index) in tarefas" :key="index">
+        {{todoList.descricao}}
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name:"TodoList",
+    props:['tarefas']
+}
+</script>
+
+<style>
+    #todoList{
+        background-color: rgb(236, 119, 9);
+        width: 500px;
+        height: 150px;
+        margin: 10px auto 0px auto;
+        display: flex;
+        flex-wrap: wrap;
+        overflow: auto; /* barra de rolagem*/
+        border: black 2px solid;
+        border-radius: 20px;
+    }
+
+    .tarefasCaixas{
+        width: (tarefas);
+        background-color: #06beb6;
+        height: 50px;
+        border: 2px solid black;
+        margin: 5px 20px 0 10px;
+        text-align: center;
+        line-height: 50px;
+        cursor: pointer;
+        color: black; 
+        overflow: hidden; /* esconde uma parte do texto dentro da caixa*/
+    }
+</style>
